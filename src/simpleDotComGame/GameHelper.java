890399eb14b50a32a518c1a5dc264f4e7c73e0e3
Axel_ST <Kozlov.axe@gmail.com>
2022-08-26 15:46:35 +0700

    Update GameHelper class file like p. 142

--- src/simpleDotComGame/GameHelper.java
+++ src/simpleDotComGame/GameHelper.java
@@ -6,5 +6,24 @@
 
 package simpleDotComGame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputline = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputline = is.readLine();
+            if (inputline.length() == 0) {
+                return null;
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return inputline;
+    }
+    
 }
